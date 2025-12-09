18027706fcc28f64cf084ca62bb772bf840deed4
Therese <twa140@sfu.ca>
2025-12-08 22:04:33 -0800

    Fixed the navigation bar and removed any duplicate code

--- main.js
+++ main.js
@@ -22,21 +22,6 @@ container.addEventListener("scroll", () => {
   });
 });
 
-
-// Source - https://stackoverflow.com/questions/7717527/smooth-scrolling-when-clicking-an-anchor-link
-// Posted by Joseph Silber, modified by community. See post 'Timeline' for change history
-// Retrieved 2025-11-19, License - CC BY-SA 3.0
-
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
-
 // Source - https://stackoverflow.com/questions/7717527/smooth-scrolling-when-clicking-an-anchor-link
 // Posted by Joseph Silber, modified by community. See post 'Timeline' for change history
 // Retrieved 2025-11-19, License - CC BY-SA 3.0
